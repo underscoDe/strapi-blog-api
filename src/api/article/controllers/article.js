a70733dde219f6fe0a01592419d258a97f2606d3
Arsene Awounou <developer.awounou@gmail.com>
2023-11-12 17:37:56 +0100

    feat: override controllers to handle custom queries

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -1,9 +1,42 @@
-'use strict';
+"use strict";
 
 /**
  * article controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController("api::article.article", ({ strapi }) => ({
+  async find(ctx) {
+    let entities;
+    if (ctx.query.hasOwnProperty("search")) {
+      console.log("search");
+      entities = await strapi.entityService.findPage("api::article.article", {
+        filters: {
+          title: {
+            $containsi: ctx.query.search,
+          },
+          publishedAt: {
+            $notNull: true,
+          },
+        },
+        populate: "*",
+        page: ctx.query.page,
+        pageSize: ctx.query.pageSize,
+      });
+    } else if (ctx.query.hasOwnProperty("slug")) {
+      console.log("slug");
+      entities = await strapi.entityService.findMany("api::article.article", {
+        filters: {
+          slug: ctx.query.slug,
+          publishedAt: {
+            $notNull: true,
+          },
+        },
+        populate: "*",
+      });
+    }
+
+    return entities;
+  },
+}));
